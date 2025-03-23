9c0f0b6ea4a2df3d0c054c685b0f4d6632f64264
SUMAN1040 <UEMJ2003@GMAIL.COM>
2025-03-23 08:13:24 +0530

    SoS alert

---
+++ app/src/main/java/com/example/oxitech/openui/FirstActivity.kt
@@ -0,0 +1,34 @@
+package com.example.oxitech.openui
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.oxitech.R
+
+class FirstActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_first)
+
+        // Finding the views by their IDs
+        val firstButton1: TextView = findViewById(R.id.first_button_1)
+        val firstButton2: Button = findViewById(R.id.first_button_2)
+
+        // Set click listeners
+        firstButton1.setOnClickListener {
+            // Add action for "How does it work?" button
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+//        firstButton2.setOnClickListener {
+//            // Add action for "Start now" button
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//        }
+    }
+}
--- app/src/main/java/com/example/oxitech/other/SplashActivity.kt
+++ app/src/main/java/com/example/oxitech/other/SplashActivity.kt
@@ -5,10 +5,12 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.example.oxitech.MainActivity
 import com.example.oxitech.R
+import com.example.oxitech.openui.FirstActivity
 
 class SplashActivity : AppCompatActivity() {
 
@@ -17,11 +19,31 @@ class SplashActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
 
+
+
         //full screen
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+
+
+//
+//        //load zoom-in animation
+//        val zoomOut = AnimationUtils.loadAnimation(this, R.anim.zoom_in)
+//        val splash_things = findViewById<ImageView>(R.id.splash_things)
+//        splash_things.startAnimation(zoomOut)
+
+
+
+
+
+        //reference to imageview
+        val splash_things = findViewById<ImageView>(R.id.splash_things)
+        //load zoomout animation
+        val zoomOutAnimation = AnimationUtils.loadAnimation(this, R.anim.zoom_in)
+        //start animation
+        splash_things.startAnimation(zoomOutAnimation)
 
 
         //media player
@@ -31,10 +53,10 @@ class SplashActivity : AppCompatActivity() {
 
         //splash screen
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            val intent = Intent(this@SplashActivity, FirstActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000)
+        }, 2000)
 
     }
 }
